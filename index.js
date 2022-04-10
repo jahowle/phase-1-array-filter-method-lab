3144c6475adc7f669c8e4e9615cbfa1ff0ce1401
Jonny Howle <jonny.howle@gmail.com>
2022-04-10 15:10:09 -0600

    commit tree ef76f77b67a6825a212b64b395e1f857071681e1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function fuzz(element) {
+
+}
+
+function findMatching(drivers, string) {
+    return drivers.filter(element => element.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(element => element.startsWith(string));
+}
+
+function matchName(driverObject, string) {
+    return driverObject.filter(element => element.name === string);
+}
